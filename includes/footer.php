cad2c4a75feb47b21eb4154938637e3dd94926fd
kwannk <157868484+kwannk@users.noreply.github.com>
2026-04-16 08:31:46 +0700

    Delete includes/footer.php

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- includes/footer.php -->
-</main>
-<footer class="bg-light text-center py-3 mt-5">
-    <p class="mb-0">💄 Beauty Store – всё для макияжа. © 2025</p>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
